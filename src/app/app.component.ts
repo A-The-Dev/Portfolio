f1f8bc7100e8bf820766d5b022e30b8dd85458a1
Antho-CodeMaster <antho.samson@outlook.fr>
2025-07-30 21:27:18 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SkillsetComponent } from './skillset/skillset.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AboutComponent,
+    //ContactsComponent,
+    //SkillsetComponent,
+    //ProjectsComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
